f8817efe32e6cf5e4f658e92dbf39f9f49d37b80
Víctor Tamayo Olea <vtamayo@uma.es>
2023-05-28 13:33:12 +0200

    Added more models to DB

--- sensors/models.py
+++ sensors/models.py
@@ -6,5 +6,21 @@ import datetime
 class Sensor(models.Model):
     type = models.IntegerField(default = 1)
     name = models.CharField(max_length = 255)
+    change_pendig = models.BooleanField(default = False)
+    schedule_active = models.BooleanField(default = False)
+
+
+class Measure(models.Model):
+    sensor = models.ForeignKey(Sensor)
+    type = models.IntegerField(default = 1)
     value = models.FloatField(default = 0)
-    date = models.DateTimeField(default = datetime.datetime.now) 
+    date = models.DateTimeField(default = datetime.datetime.now)
+
+
+class schedule(models.Model):
+    sensor = models.ForeignKey(Sensor)
+    month_days = models.IntegerField(default = 0)
+    week_days = models.IntegerField(default = 0)
+    hour = models.IntegerField(default = 0)
+    minute = models.IntegerField(default = 0)
+    active = models.BooleanField(default = False)
